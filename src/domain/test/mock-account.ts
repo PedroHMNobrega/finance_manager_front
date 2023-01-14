bb86b4c353c62dcdbcb1650622b4ed8654c36ec4
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-13 22:07:52 -0300

    test: ensure RemoteAuthentication returns an account on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,6 +1,11 @@
 import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AccountModel } from '@/domain/models/account-model'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: 'any-email',
   password: 'any-password'
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: 'any-token'
 })
