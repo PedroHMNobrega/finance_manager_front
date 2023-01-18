46a7a7b61296d3e08f52c0690d3503b17a7ebfb5
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-18 13:19:22 -0300

    feat: ensure login calls authentication with correct values

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
-import { Validation } from '@/presentation/protocols/validation'
-
-class Valid implements Validation {
-  validate (fieldName: string, fieldValue: string): string {
-    return null
-  }
-}
 
 const Router: React.FC = () => {
-  const valid = new Valid()
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login validation={valid} />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
