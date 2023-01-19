1559dd48309708f733c32e1912df6b2473ad68e6
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-19 16:47:55 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
+
+  it('should return falsy if field is empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate('any-email')
+    expect(error).toBeFalsy()
+  })
 })
