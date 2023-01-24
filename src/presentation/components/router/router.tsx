7a3c13b0c9c1e9150a9550b26c53577d88cb46c0
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-01-24 14:46:49 -0300

    feat: create NotFound page

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFound from '@/presentation/pages/not-found/not-found'
 
 type Props = {
   makeLogin: () => React.ReactNode
@@ -12,6 +13,7 @@ const Router: React.FC<Props> = ({ makeLogin }: Props) => {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={loginComponent} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
