0edbd1a562d2c732f235cbe903c7f8fe6d0f76f0
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-02-19 14:46:02 -0300

    test: add create-purchase usecase

---
+++ tests/data/usecases/remote-create-purchase.spec.ts
@@ -0,0 +1,71 @@
+import { RemoteCreatePurchase } from '@/data/usecases/purchase/remote-create-purchase'
+import { HttpClientSpy } from '@/tests/data/mocks'
+import { Purchase } from '@/domain/models'
+import { mockCreatePurchaseParams, mockPurchase } from '@/tests/domain/mocks/mock-purchase'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+
+type SutTypes = {
+  sut: RemoteCreatePurchase
+  httpClientSpy: HttpClientSpy<Purchase>
+}
+
+const url = 'any-url'
+
+const makeSut = (): SutTypes => {
+  const httpClientSpy = new HttpClientSpy<Purchase>()
+  const sut = new RemoteCreatePurchase(url, httpClientSpy)
+  return {
+    sut,
+    httpClientSpy
+  }
+}
+
+describe('RemoteCreatePurchase', () => {
+  it('should call HttpClient with correct values', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    const params = mockCreatePurchaseParams()
+    await sut.create(params)
+
+    expect(httpClientSpy.url).toBe(url)
+    expect(httpClientSpy.method).toBe('post')
+    expect(httpClientSpy.headers).toEqual({
+      Authorization: `Bearer ${params.token}`
+    })
+  })
+
+  it('should throw InvalidCredientialsError if HttpClient returns 401', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized
+    }
+    const params = mockCreatePurchaseParams()
+    const promise = sut.create(params)
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+
+  it.each([
+    { statusCode: HttpStatusCode.badRequest },
+    { statusCode: HttpStatusCode.serverError },
+    { statusCode: HttpStatusCode.notFound }
+  ])('Should throw UnexpectedError if HttpClient returns other status code', async ({ statusCode }) => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode
+    }
+    const promise = sut.create(mockCreatePurchaseParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('Should return Purchase if HttpClient returns 200', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    const httpResult = mockPurchase(1)
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const params = mockCreatePurchaseParams()
+    const result = await sut.create(params)
+    await expect(result).toStrictEqual(httpResult)
+  })
+})
--- tests/domain/mocks/mock-purchase.ts
+++ tests/domain/mocks/mock-purchase.ts
@@ -1,25 +1,26 @@
 import { LoadPurchaseListParams } from '@/domain/usecases'
 import { Purchase } from '@/domain/models'
+import { CreatePurchaseParams } from '@/domain/usecases/create-purchase'
 
 export const mockLoadPurchaseListParams = (): LoadPurchaseListParams => ({
   token: 'any-token'
 })
 
+export const mockCreatePurchaseParams = (): CreatePurchaseParams => ({
+  purchase: mockPurchase(null),
+  token: 'any-token'
+})
+
+export const mockPurchase = (id): Purchase => ({
+  id: id,
+  name: 'any-name',
+  installmentsNumber: 10,
+  value: 100,
+  category: 1,
+  firstInstallmentDate: '2023-02-19'
+})
+
 export const mockPurchaseList = (): Purchase[] => ([
-  {
-    id: 1,
-    name: 'any-name1',
-    installmentsNumber: 10,
-    value: 100,
-    category: 1,
-    firstInstallmentData: '2023-02-19'
-  },
-  {
-    id: 2,
-    name: 'any-name2',
-    installmentsNumber: 1,
-    value: 10,
-    category: 2,
-    firstInstallmentData: '2022-01-10'
-  }
+  mockPurchase(1),
+  mockPurchase(2)
 ])
