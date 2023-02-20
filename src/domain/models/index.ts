1438771d9c7bed41226133908e747264f58d7d18
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-02-20 14:16:12 -0300

    refactor: update index.ts

--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,2 +1,3 @@
 export * from './account-model'
 export * from './purchase'
+export * from './category'
---
+++ src/domain/usecases/purchase/index.ts
@@ -0,0 +1,3 @@
+export * from './load-purchase-list'
+export * from './create-purchase'
+export * from './delete-purchase'
