1cf092cfa7921b36ccf3251daf86c320aa914dbe
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-02-27 20:07:36 -0300

    feat: create category-modal component

---
+++ src/presentation/pages/purchases/components/category-modal/category-modal.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Styles from './category-modal-styles.scss'
+import { AddButton, DeleteButton, Modal } from '@/presentation/components'
+import { Category } from '@/domain/models'
+import Container from '@/presentation/components/container/container'
+
+const CategoryModal: React.FC = () => {
+  const categories: Category[] = [
+    {
+      id: 1,
+      name: 'Tech'
+    },
+    {
+      id: 2,
+      name: 'Comida'
+    }
+  ]
+
+  const renderCategories = (): JSX.Element => {
+    if (categories.length === 0) {
+      return (
+        <h1 className={Styles.no_categories}>Criar Categoria</h1>
+      )
+    } else {
+      return (
+        <div className={Styles.categories}>
+          {categories.map(category => (
+            <div key={category.id} className={Styles.category_container}>
+              <span>{category.name}</span>
+              <DeleteButton id={category.id} callback={(id) => console.log(id)}/>
+            </div>
+          ))}
+        </div>
+      )
+    }
+  }
+
+  return (
+    <Modal title={'Categorias'}>
+      <Container className={Styles.container}>
+        <AddButton action={() => {
+          console.log('add-category')
+        }} />
+        {renderCategories()}
+      </Container>
+    </Modal>
+  )
+}
+
+export default CategoryModal
