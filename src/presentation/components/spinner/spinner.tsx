22f694dd60afaa8e0670357b195c12c365a3ac3f
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-03-30 13:42:26 -0300

    feat: change spinner size dynamically

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import Styles from './spinner-styles.scss'
+
+export enum SpinnerSize {
+  BIG = 1,
+  MEDIUM = 2
+}
 
 type Props = {
   className: string
+  size?: SpinnerSize
 }
 
+type CustomCSSProperties = CSSProperties & { '--size'?: string }
+
 const Spinner: React.FC<Props> = (props: Props) => {
+  const size = props.size ? props.size : SpinnerSize.BIG
+
+  const style: CustomCSSProperties = {
+    '--size': size.toString()
+  }
+
   return (
-    <div {...props} data-testid="spinner" className={[Styles.spinner, props.className].join(' ')}>
+    <div
+      {...props}
+      data-testid="spinner"
+      className={[Styles.spinner, props.className].join(' ')}
+      style={style}
+    >
       <div></div>
       <div></div>
       <div></div>
--- src/presentation/components/with-loading/with-loading.tsx
+++ src/presentation/components/with-loading/with-loading.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Spinner } from '@/presentation/components'
+import { SpinnerSize } from '@/presentation/components/spinner/spinner'
 
 type Props = {
   children: React.ReactNode
   loadingClass: string
   loading: boolean
+  size?: SpinnerSize
 }
 
 const WithLoading: React.FC<Props> = (props: Props) => {
   return (
     <div data-testid="with-loading">
-      { props.loading && <Spinner className={props.loadingClass} />}
+      { props.loading && <Spinner className={props.loadingClass} size={props.size} />}
       { !props.loading && props.children}
     </div>
   )
