95890becca31e9ce4c288f3625f04293f5ba1cec
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-03-30 14:03:45 -0300

    refactor: allow loading on delete button

--- src/presentation/components/delete-button/delete-button.tsx
+++ src/presentation/components/delete-button/delete-button.tsx
@@ -2,19 +2,24 @@ import React from 'react'
 import Styles from './delete-button-styles.scss'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { WithLoading } from '@/presentation/components'
+import { SpinnerSize } from '@/presentation/components/spinner/spinner'
 
 type Props = {
   id: number
   callback: Function
+  loading?: boolean
 }
 
-const DeleteButton: React.FC<Props> = (props: Props) => {
+const DeleteButton: React.FC<Props> = ({ id, callback, loading = false }: Props) => {
   const handleClick: React.MouseEventHandler<SVGSVGElement> = (): void => {
-    props.callback(props.id)
+    callback(id)
   }
 
   return (
-    <FontAwesomeIcon icon={faTrash} className={Styles.icon} onClick={handleClick} data-testid="delete-button" id={`${props.id}`} />
+    <WithLoading loadingClass={Styles.loading} loading={loading} size={SpinnerSize.MEDIUM}>
+      <FontAwesomeIcon icon={faTrash} className={Styles.icon} onClick={handleClick} data-testid="delete-button" id={`${id}`} />
+    </WithLoading>
   )
 }
 
