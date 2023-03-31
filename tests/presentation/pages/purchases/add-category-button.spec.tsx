ad383e5f41bea87e9f7c172ffc331d332dd82cc9
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-03-31 15:04:41 -0300

    test: use render with provider on add-category-button

--- tests/presentation/pages/purchases/add-category-button.spec.tsx
+++ tests/presentation/pages/purchases/add-category-button.spec.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
-import { act, fireEvent, render, screen } from '@testing-library/react'
+import { act, fireEvent, screen } from '@testing-library/react'
 import AddCategoryButton from '@/presentation/pages/purchases/components/add-category-button/add-category-button'
+import { renderWithProvider } from '@/tests/presentation/mocks'
+import { SagaUseCases } from '@/presentation/store/reducers/root-saga'
+import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore'
 
-const makeSut = (): void => {
-  render(
+type SutType = {
+  renderScreen: Function
+  sagaUsecases: SagaUseCases
+  store: ToolkitStore
+}
+
+const makeSut = (): SutType => {
+  const Page: React.FC = () => (
     <AddCategoryButton />
   )
+
+  const { store, sagaUsecases, renderScreen } = renderWithProvider({ Page })
+
+  return {
+    renderScreen,
+    sagaUsecases,
+    store
+  }
 }
 
 describe('AddCategoryButton Component', () => {
   describe('Open and Close', () => {
     it('should start rendering button, but not rendering create category input', () => {
-      makeSut()
+      const { renderScreen } = makeSut()
+      renderScreen()
 
       const addButton = screen.queryByTestId('add-button')
       expect(addButton).toBeTruthy()
@@ -21,7 +39,8 @@ describe('AddCategoryButton Component', () => {
     })
 
     it('should display create category input on button click', () => {
-      makeSut()
+      const { renderScreen } = makeSut()
+      renderScreen()
 
       const addButton = screen.queryByTestId('add-button')
       act(() => {
@@ -33,7 +52,8 @@ describe('AddCategoryButton Component', () => {
     })
 
     it('should close create category input on button click', () => {
-      makeSut()
+      const { renderScreen } = makeSut()
+      renderScreen()
 
       const addButton = screen.queryByTestId('add-button')
       act(() => {
