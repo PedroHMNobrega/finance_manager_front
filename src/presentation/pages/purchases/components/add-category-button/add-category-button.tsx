744551d1ac38544aae516d78e08916017f815eb2
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-03-31 14:34:45 -0300

    feat: ensure add-category-button to open and close create input

--- src/presentation/pages/purchases/components/add-category-button/add-category-button.tsx
+++ src/presentation/pages/purchases/components/add-category-button/add-category-button.tsx
@@ -4,16 +4,23 @@ import { AddButton } from '@/presentation/components'
 
 const AddCategoryButton: React.FC = () => {
   const [category, setCategory] = useState('')
+  const [showCreateCategory, setShowCreateCategory] = useState(false)
+
+  const handleAddButtonClick = (): void => {
+    setShowCreateCategory(!showCreateCategory)
+  }
 
   return (
     <div className={Styles.addCategoryButtonContainer}>
-      <AddButton action={() => {
-        console.log('add-category')
-      }} />
-      <div className={Styles.createCategoryWrapper}>
-        <input value={category} onChange={(event) => setCategory(event.target.value)} required />
-        <button>Criar</button>
+      <div className={Styles.addButtonContainer}>
+        <AddButton action={handleAddButtonClick} />
       </div>
+      {showCreateCategory && (
+        <div className={Styles.createCategoryWrapper} data-testid="create-category-wrapper">
+          <input value={category} onChange={(event) => setCategory(event.target.value)} required />
+          <button>Criar</button>
+        </div>
+      )}
     </div>
   )
 }
