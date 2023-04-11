ac40c70631ee748dce88aaa5e20795314f95ff75
PedroHMNobrega <pedrohmnobrega@gmail.com>
2023-04-11 20:25:41 -0300

    feat: create installment update modal

--- src/presentation/pages/installments/components/installment-table/installment-table.tsx
+++ src/presentation/pages/installments/components/installment-table/installment-table.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './installment-table-styles.scss'
 import { Installment } from '@/domain/models'
+import InstallmentUpdateModal
+  from '@/presentation/pages/installments/components/installment-update-modal/installment-update-modal'
 
 type Props = {
   installments: Installment[]
 }
 
 const InstallmentTable: React.FC<Props> = ({ installments }: Props) => {
+  const [openIntallmentModal, setOpenInstallmentModal] = useState(false)
+  const [selectedInstallment, setSelectedInstallment] = useState(null)
+
+  const handleSelectInstallment = (installment): void => {
+    setSelectedInstallment(installment)
+    setOpenInstallmentModal(true)
+  }
+
   const getRowClass = (installment): string => {
     const classes = [Styles.row]
     if (installment.paid) {
@@ -31,7 +41,12 @@ const InstallmentTable: React.FC<Props> = ({ installments }: Props) => {
             <h3>Status</h3>
           </div>
           {installments.map(installment => (
-            <div key={installment.id} className={getRowClass(installment)} data-testid="installment">
+            <div
+              key={installment.id}
+              className={getRowClass(installment)}
+              data-testid="installment"
+              onClick={() => { handleSelectInstallment(installment) }}
+            >
               <h3>{installment.purchase.name}</h3>
               <h3>{installment.category ? installment.category : '-'}</h3>
               <h3>{`${installment.number}/${installment.purchase.installmentsNumber}`}</h3>
@@ -46,6 +61,13 @@ const InstallmentTable: React.FC<Props> = ({ installments }: Props) => {
 
   return (
     <div className={Styles.installments} data-testid="installment-table">
+      {openIntallmentModal && (
+        <InstallmentUpdateModal
+          setOpen={setOpenInstallmentModal}
+          selected={selectedInstallment}
+          setSelected={setSelectedInstallment}
+        />
+      )}
       {renderTable()}
     </div>
   )
---
+++ src/presentation/pages/installments/components/installment-update-modal/installment-update-modal.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Styles from './installment-update-modal-styles.scss'
+import { Input, Modal, SubmitButton } from '@/presentation/components'
+import { FormContext } from '@/presentation/contexts'
+import { Installment } from '@/domain/models'
+
+type Props = {
+  setOpen: Function
+  selected: Installment
+  setSelected: Function
+}
+
+const InstallmentUpdateModal: React.FC<Props> = ({ setOpen, selected, setSelected }: Props) => {
+  const [state, setState] = useState({
+    value: selected.value_paid,
+    valueError: '',
+    mainError: ''
+  })
+
+  return (
+    <Modal title={'Pagar Parcela'} setOpen={setOpen} name='update-installment-modal'>
+      <div className={Styles.info_container}>
+        <span><b>Compra:</b> {selected.purchase.name} - {selected.number}/{selected.purchase.installmentsNumber}</span>
+      </div>
+      <FormContext.Provider value={{ state, setState }}>
+        <form className={Styles.form}>
+          <div className={Styles.input_container}>
+            <Input required type="money" name="value" label="Valor" />
+          </div>
+          <div className={Styles.button_container}>
+            <SubmitButton title={'Pagar'} disabled={false} className={Styles.button} />
+          </div>
+        </form>
+      </FormContext.Provider>
+    </Modal>
+  )
+}
+
+export default InstallmentUpdateModal
